8c47c0858322a65365dc448f3a395350982569d1
DaiDuong <duongsdbg1102@gmail.com>
2022-04-08 17:54:53 +0700

    update darkMode

--- js/main.js
+++ js/main.js
@@ -9,7 +9,9 @@ document.querySelector('#checkbox').addEventListener('click',function(){
 window.onscroll = function () {
         //code taoj hieu ung truot header khi scroll
         if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-            document.getElementById("headerMain").style.background = " #fff";
+        //     document.getElementById("headerMain").style.background = " #fff";
+            document.getElementById("headerMain").style.background = " #072026";
+
             document.getElementById("headerMain").style.boxShadow = " 0 5px 15px rgba(0,0,0,0.1)";
             document.getElementById("headerMain").style.transform = "  translateY(0)";
         } else {
